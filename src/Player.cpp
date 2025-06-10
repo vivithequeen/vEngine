8b34dfab3b2f0b4517a1ca6fdcc726429dbbaf38
vivithequeen <vivithequeen1@gmail.com>
2025-06-10 19:22:40 -0400

    textured cube!!

---
+++ src/Player.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "raylib.h"
+#include "rcamera.h"
+#include "resource_dir.h"
+
+using namespace std;
+
+class Player
+{
+public:
+    Camera3D camera;
+    const float WALKSPEED = 7.0f;
+
+    Vector3 velocity = {0,0,0};
+    Player()
+    {
+        camera = {0};
+        camera.position = (Vector3){0.0f, 2.0f, 4.0f};
+        camera.target = (Vector3){0.0f, 2.0f, 0.0f};
+        camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+        camera.fovy = 60.0f;
+        camera.projection = CAMERA_PERSPECTIVE;
+    }
+
+    void process(float dt)
+    {
+        velocity.x = (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) * WALKSPEED*dt -
+                      (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) * WALKSPEED*dt;
+        velocity.y = (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) * WALKSPEED*dt -
+                      (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) * WALKSPEED*dt;
+        // velocity.z += 0.0f; // No up-down movement
+
+        UpdateCameraPro(&camera,
+                        velocity,
+                        (Vector3){
+                            GetMouseDelta().x * 0.05f, // Rotation: yaw
+                            GetMouseDelta().y * 0.05f, // Rotation: pitch
+                            0.0f                       // Rotation: roll
+                        },
+                        GetMouseWheelMove() * 2.0f);
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include "raylib.h"
 #include "rcamera.h"
 #include "resource_dir.h"	
-#include "RayCastLogic.cpp"
+#include "raymath.h"
+
+#include "Player.cpp"
 using namespace std;
 
 int main ()
@@ -18,47 +20,34 @@ int main ()
 	SearchAndSetResourceDir("resources");
 
 
+	Material test = LoadMaterialDefault();
+	Texture texture;
+	texture = LoadTexture("testMeow.png");
+	SetMaterialTexture(&test, MATERIAL_MAP_DIFFUSE, texture);
 
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 0.0f, 2.0f, 4.0f };    // Camera position
-    camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 60.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE; 
+
+	Player player;
 	HideCursor();
 	DisableCursor();
 	// game loop
-	
+	Mesh m = GenMeshCube(3,3,3);
+
+	Matrix matrix = MatrixTranslate(0.0f, 3.0f, 0.0f);
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
 		// drawing
-        UpdateCameraPro(&camera,
-            (Vector3){
-                (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))*0.1f -      // Move forward-backward
-                (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))*0.1f,    
-                (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))*0.1f -   // Move right-left
-                (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))*0.1f,
-                0.0f                                                // Move up-down
-            },
-            (Vector3){
-                GetMouseDelta().x*0.05f,                            // Rotation: yaw
-                GetMouseDelta().y*0.05f,                            // Rotation: pitch
-                0.0f                                                // Rotation: roll
-            },
-            GetMouseWheelMove()*2.0f);     
+		player.process(GetFrameTime());
 		BeginDrawing();
-
 		ClearBackground(BLACK);
-		BeginMode3D(camera);
+		BeginMode3D(player.camera);
+		
 		DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
         DrawCube((Vector3){ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
         DrawCube((Vector3){ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
         DrawCube((Vector3){ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD); 		
 		
+		DrawMesh(m, test, matrix);
 
-		//rayCastLogic.process(GetFrameTime());
-		
-		//DrawText("get hacked",0,0,18,WHITE);
 		EndMode3D();
 		DrawFPS(0,0);
 		EndDrawing();
@@ -67,7 +56,8 @@ int main ()
 	// cleanup
 	// unload our texture so it can be cleaned up
 
-
+	UnloadMaterial(test);
+	UnloadTexture(texture);
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
